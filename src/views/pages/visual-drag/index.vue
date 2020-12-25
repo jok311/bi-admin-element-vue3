47535b503f23f7342d9e99f0b6b1e8227583a652
陈兆齐 <zhaoqi@inconnecting.com>
2020-12-25 19:02:59 +0800

    拖拽

--- src/views/pages/visual-drag/index.vue
+++ src/views/pages/visual-drag/index.vue
@@ -6,103 +6,164 @@
         </div>
     </div>
 
-
-    <div class="content" @drop="handleDrop" @dragover="handleDragOver">
+    <!-- width: `${item.style.width/editContentBoxWidth < 1 ? item.style.width/editContentBoxWidth*100 : 100}%`, -->
+    <div id="edit-content-box" class="content" @drop="handleDrop($event)" @dragover="handleDragOver">
         <div 
           class="list" 
           v-for="(item, index) in editComponentList" 
-          :key="index" 
-          :style="{ top: item.style.top+'px', left: item.style.left+'px'}"
-          @mousedown="handleMouseDownOnPoint(item, index)"
-
+          :key="index"
+          :style="{ 
+            top: item.style.top+'px', 
+            left: item.style.left+'px',
+            height: item.style.height+'px',
+            width: `${item.style.width}px`,
+          }"
+          
+          @mousedown="handleMouseDown(item, index)"
         >
+          <div class="point-box" @mousedown="handleMouseDownOnPoint(item, index)"><span class="point"></span></div>
           {{ item.label }}
+          {{ editContentBoxWidth }}
+          ---
+          {{ item.style.width }}
+          ----
+          {{ item.style.width/editContentBoxWidth }}
         </div>
     </div>    
   </div>
 </template>
 
 <script>
-import { ref, reactive, unref, watch } from 'vue'
+import { ref, reactive, unref, watch, onMounted } from 'vue'
 export default {
   setup() {
+    let colNum = 12, colWidth=ref(null), rowHeight = 150
     let componentList = ref([
-      {label: 'chart', key: 'chart'},
-      {label: 'pie', key: 'pie'},
-      {label: 'line', key: 'line'},
+      {
+        label: 'chart', 
+        key: 'chart', 
+        style: {
+          height: 72,
+          width: 333
+        }
+      },
+      {
+        label: 'pie', 
+        key: 'pie',
+        style: {
+          height: 72,
+          width: 333
+        }        
+      },
+      {
+        label: 'line', 
+        key: 'line',
+        style: {
+          height: 72,
+          width: 333
+        }        
+      },
     ])
+
+    let editContentBox, editContentBoxWidth =ref(null); //编辑区域的宽高位置
+    onMounted( () => {
+      editContentBox = document.querySelector('#edit-content-box').getBoundingClientRect()
+      editContentBoxWidth.value = editContentBox.width
+      colWidth.value = editContentBoxWidth.value/colNum
+      console.log(colWidth.value, 6767667)
+    })
 
     let editComponentList = window.localStorage.editComponentList ? ref(JSON.parse(window.localStorage.editComponentList)) : ref([])
 
+    //组件拖拽区target
+    let dropOffsetX, dropOffsetY, dropScreenY
     function handleDragStart(e) {
       console.log(e, 36363663)
       e.dataTransfer.setData('index', e.target.dataset.index)
+      dropOffsetX = e.offsetX
+      dropOffsetY = e.offsetY
+      dropScreenY = e.screenY
     }
 
+    
     function handleDrop(e) {
-      e.preventDefault()
-      e.stopPropagation()
+      // e.preventDefault()
+      // e.stopPropagation()
       if( !e.dataTransfer.getData('index') ) return
       const component = { ...componentList.value[e.dataTransfer.getData('index')] } //深拷贝
+      let targetName = e.target.id == 'edit-content-box'
       component['style'] = {
-        top: e.offsetY,
-        left: e.offsetX
+        top: targetName ? e.offsetY - dropOffsetY : e.y - dropOffsetY,
+        left: targetName ? e.offsetX - dropOffsetX : e.x - dropOffsetX - dropScreenY,
+        ...component.style
       }
       editComponentList.value.push(component)
-      // window.localStorage.editComponentList = JSON.stringify(editComponentList.value)
+      window.localStorage.editComponentList = JSON.stringify(editComponentList.value)
       editHistory(editComponentList)
-      console.log(555544444)
     }
 
     let dragable = true
     function handleDragOver(e) {
       e.preventDefault();
-      if( dragable ) {
-        dragable = false
-        setTimeout( () => {
-          dragable = true
-        }, 500)
-      }
+      e.stopPropagation()
+      // if( dragable ) {
+      //   dragable = false
+      //   setTimeout( () => {
+      //     dragable = true
+      //   }, 500)
+      // }
     }
 
     function handleMouseDown(e) {
       console.log(e, 6464646)
       e.preventDefault()
       e.stopPropagation()
-
     }
 
-    function handleMouseDownOnPoint(point, index) {
-      console.log(point, 777774444)
+    function handleMouseDown(item, index) {
       const downEvent = window.event
       downEvent.stopPropagation()
       downEvent.preventDefault()
-      const pos = point
-      console.log(pos, 79797979)
+      const pos = item
       const startY = downEvent.clientY
       const startX = downEvent.clientX
       // 如果直接修改属性，值的类型会变为字符串，所以要转为数值型
       const startTop = Number(pos.style.top)
       const startLeft = Number(pos.style.left)
+      const height = Number(pos.style.height)
+      const width = Number(pos.style.width)
 
-      let moveable = true
+      let moveable = true, isMove = false
       const move = (moveEvent) => {
         if(!moveable) return
+        isMove = true //移动了
         const currX = moveEvent.clientX
         const currY = moveEvent.clientY
         console.log(startY, startX, currY, currX, startTop, startLeft) 
-        editComponentList.value[index].style = { top:  currY - startY + startTop, left: currX - startX + startLeft}
-        moveable = false
-        setTimeout(() => {
-          moveable = true
-        }, 50);
-        // this.$store.commit('setShapeStyle', pos)
+        let newTop = currY - startY + startTop
+        let newLeft = currX - startX + startLeft
+        let maxLeft = editContentBoxWidth.value - width
+        newLeft = newLeft > maxLeft ? maxLeft : newLeft
+        editComponentList.value[index].style = { 
+          top:  newTop >= 0 ? newTop : 0, 
+          left: newLeft >= 0 ? newLeft : 0,
+          height,
+          width
+        }
+        // moveable = false
+        // setTimeout(() => {
+        //   moveable = true
+        // }, 50);
       }
       const up = () => {
           document.removeEventListener('mousemove', move)
           document.removeEventListener('mouseup', up)
-          window.localStorage.editComponentList = JSON.stringify(editComponentList.value)
-          editHistory(editComponentList)
+          console.log(isMove, 16161616)
+          if(isMove) {
+            editHistory(editComponentList)
+            isMove = false
+          }
+          
           console.log(105555)
           // needSave && this.$store.commit('recordSnapshot')
       }
@@ -110,31 +171,72 @@ export default {
       document.addEventListener('mouseup', up)
     }
 
+    function handleMouseDownOnPoint(item, index) {
+      const downEvent = window.event
+      downEvent.stopPropagation()
+      downEvent.preventDefault()
+      const pos = item
+      const startY = downEvent.clientY
+      const startX = downEvent.clientX
+      // 如果直接修改属性，值的类型会变为字符串，所以要转为数值型
+      const startTop = Number(pos.style.top)
+      const startLeft = Number(pos.style.left)  
+      const startHeight = Number(pos.style.height)
+      const startWidth = Number(pos.style.width)
+      
+      let isResize = false
+      const move = (moveEvent) => {
+        isResize = true
+        const currX = moveEvent.clientX
+        const currY = moveEvent.clientY
+        const disY = currY - startY
+        const disX = currX - startX
+        let newWidth = startWidth + disX
+        let MaxWidth = editContentBoxWidth.value - startLeft
+        newWidth = newWidth > MaxWidth ? MaxWidth : newWidth
+        let style = { 
+          top:  startTop, 
+          left: startLeft,
+          width: newWidth,
+          height: startHeight + disY
+        }
+        editComponentList.value[index].style = { ...style }
+      }
+      const up = () => {
+        document.removeEventListener('mousemove', move)
+        document.removeEventListener('mouseup', up)
+        if(isResize) {
+          editHistory(editComponentList)
+          console.log(editComponentList, 190190190)
+        }
+      }
+      document.addEventListener('mousemove', move)
+      document.addEventListener('mouseup', up)      
+    }
+
     function editHistory(editComponentList) {
+      window.localStorage.editComponentList = JSON.stringify(editComponentList.value)
       let editComponentListHistory = []
       if( window.localStorage.editComponentListHistory ) {
         editComponentListHistory = JSON.parse(window.localStorage.editComponentListHistory)
-        console.log(editComponentListHistory, 1111111999999)
         editComponentListHistory.push(editComponentList.value)
       }else{
         editComponentListHistory = [editComponentList.value]
-        console.log(editComponentList, 121212122)
-
       }
-      console.log(editComponentListHistory, 111177777)
       window.localStorage.editComponentListHistory = JSON.stringify(editComponentListHistory)
     }
 
-    watch( 
-      () => editComponentList,
-      val => {
-        // console.log(val, 1115555)
-      },
-      { immediate: true, deep: true },     
-    )
 
-
-    return { componentList, editComponentList, handleDragStart, handleDrop, handleDragOver, handleMouseDownOnPoint } 
+    return {
+      colWidth,
+      editContentBoxWidth,
+      componentList, 
+      editComponentList, 
+      handleDragStart,
+      handleDrop,
+      handleDragOver,
+      handleMouseDown,
+      handleMouseDownOnPoint } 
   }
 }
 </script>
@@ -154,9 +256,31 @@ export default {
     height 98vh
     flex 1
     position relative
+    overflow-y auto
+    overflow-x hidden
     .list
       background #eee
       height 72px
-      width 33%
+      // width 33%
       position absolute
+      border 1px solid #409EFF
+      box-sizing border-box
+      .point-box
+        cursor se-resize
+        display inline-block
+        position absolute
+        width 12px
+        height 12px
+        background #eee
+        right 0
+        bottom 0
+        .point
+          display inline-block
+          border-right 3px solid #409EFF
+          border-bottom 3px solid #409EFF
+          width 5px
+          height 5px
+          position absolute
+          right 2px
+          bottom 2px
 </style>
